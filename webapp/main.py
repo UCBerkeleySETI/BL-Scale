a1070c4b9493d4173ea6ea42febd21895b6517bb
Peter Ma` <shadowfax0230@gmail.com>
2020-06-30 23:09:26 -0400

    zmq test

--- webapp/main.py
+++ webapp/main.py
@@ -9,14 +9,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 import zmq
-
 import logging
 from google.cloud import storage
-
 from flask import render_template, request, redirect, session, Flask
-
+import time
 import os
 
 global cache
@@ -101,6 +98,28 @@ def forgot_password():
 ####################################################################################################
 # ___________________________________END OF USER AUTHENTICATIONS___________________________________#
 ####################################################################################################
+@app.route('/zmq', methods=['GET', 'POST'])
+def zmq_test():
+    print("IN ZMQ FUNCTION")
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:5555")
+    print("ZMQ SOCKET BINDED TO 5555")
+    stuff=[]
+    while True:
+        print("Waiting for message")
+        #  Wait for next request from client
+        message = socket.recv()
+        print("Received request: %s" % message)
+        stuff.append(message)
+        print("GOT message!")
+        #  Do some 'work'
+        time.sleep(1)
+
+        #  Send reply back to client
+        socket.send(b"World")
+        return render_template("zmq_test.html", title="Main Page", sample_urls=stuff)
+    return render_template("zmq_test.html", title="Main Page", sample_urls=stuff)
 
 
 @app.route('/home', methods=['GET', 'POST'])
