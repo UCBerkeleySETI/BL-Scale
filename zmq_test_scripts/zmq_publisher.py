eda403c8910041a5c0a32e8b51c0e7d6a0d61852
Peter Ma` <shadowfax0230@gmail.com>
2020-07-02 20:29:00 -0400

    Update zmq_publisher.py

--- zmq_test_scripts/zmq_publisher.py
+++ zmq_test_scripts/zmq_publisher.py
@@ -5,14 +5,14 @@ context = zmq.Context()
 socket = context.socket(zmq.PUB)
 socket.bind("tcp://*:5000")
 
-messages = [100,200,300]
+messages = [100,200,300,400,500, 600, 700, 800]
 curMsg = 0
 
 while True:
     sleep(1)
     print("Sending message...")
     socket.send_pyobj({curMsg:messages[curMsg]})
-    if curMsg ==2:
+    if curMsg ==7:
         curMsg = 0
     else:
         curMsg= curMsg + 1
