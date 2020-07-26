ac3212861b147458babc4d61f630882d4fabb9b3
FX196 <mikechen212@gmail.com>
2020-07-26 17:15:27 -0400

    try redis

---
+++ webapp/redis_test.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_session import Session
+from flask import session
+
+
+sess = Session()
+
+app = Flask(__name__, instance_relative_config=False)
+app.config.from_object('config.Config')
+
+def create_app():
+
+    sess.init_app(app)
+
+    with app.app_context():
+        return app
+
+@app.route('/')
+def index():
+    if "count" in session:
+        session["count"]+=1
+    else:
+        session["count"]=1
+    return str(count)
