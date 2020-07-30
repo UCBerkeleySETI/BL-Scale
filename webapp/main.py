ad0dfa1a89774ff5f264a86dd6b3c9c1f48dfa5f
Peter Ma` <shadowfax0230@gmail.com>
2020-07-30 17:34:05 -0400

    fixed the display errors

--- webapp/main.py
+++ webapp/main.py
@@ -301,63 +301,58 @@ def zmq_sub():
             try:
                 test_login = check_if_login()
                 session["results_counter"]+=1
-                print(session["results_counter"]*3)
+                
                 message_dict = db.child("breakthrough-listen-sandbox").child("flask_vars").child("processed_observations").child("Energy-Detection").order_by_child("timestamp").limit_to_last(3*session["results_counter"]).get().val()
+                print(message_dict)
                 db_cache_keys = []
                 retrieve_cache = db.child("breakthrough-listen-sandbox").child("flask_vars").child("cache").get()
                 for rc in retrieve_cache.each():
                     db_cache_keys += [str(rc.key())]
                 print(db_cache_keys)
-             
-                if not cache:
-                    print("Cache empty")
-                    for key in message_dict:
-                        cache[key] = get_processed_hist_and_img(message_dict[key]["object_uri"]+"/info_df.pkl")
-                        db.child("breakthrough-listen-sandbox").child("flask_vars").child("cache").child(key).set(cache[key])
-                else:
-                    print("cache all updated")
+            
+                for key in message_dict:
+                    cache[key] = get_processed_hist_and_img(message_dict[key]["object_uri"]+"/info_df.pkl")
+                    db.child("breakthrough-listen-sandbox").child("flask_vars").child("cache").child(key).set(cache[key])
             except:
                 alert="invalid number"
+            print(session["results_counter"]*3)
             return render_template("zmq_sub.html", title="Main Page", message_sub=message_dict,  sample_urls = cache ,test_login = True)
         else:
         
             session["results_counter"]+=1
-            print(session["results_counter"]*3)
+            
             message_dict = db.child("breakthrough-listen-sandbox").child("flask_vars").child("processed_observations").child("Energy-Detection").order_by_child("timestamp").limit_to_last(3*session["results_counter"]).get().val()
-        
+            print(message_dict)
             db_cache_keys = []
             retrieve_cache = db.child("breakthrough-listen-sandbox").child("flask_vars").child("cache").get()
             for rc in retrieve_cache.each():
                 db_cache_keys += [str(rc.key())]
             print(db_cache_keys)
             
-            if not cache:
-                print("Cache empty")
-                for key in message_dict:
-                    cache[key] = get_processed_hist_and_img(message_dict[key]["object_uri"]+"/info_df.pkl")
-                    db.child("breakthrough-listen-sandbox").child("flask_vars").child("cache").child(key).set(cache[key])
-            else:
-                print("cache all updated")
+           
+            for key in message_dict:
+                cache[key] = get_processed_hist_and_img(message_dict[key]["object_uri"]+"/info_df.pkl")
+                db.child("breakthrough-listen-sandbox").child("flask_vars").child("cache").child(key).set(cache[key])
+            
+            print(session["results_counter"]*3)
             return render_template("zmq_sub.html", title="Main Page", message_sub=message_dict,  sample_urls = cache ,test_login = False)
     except:
         
         session["results_counter"]+=1
-        print(session["results_counter"]*3)
         message_dict = db.child("breakthrough-listen-sandbox").child("flask_vars").child("processed_observations").child("Energy-Detection").order_by_child("timestamp").limit_to_last(3*session["results_counter"]).get().val()
-    
+        print(message_dict)
         db_cache_keys = []
         retrieve_cache = db.child("breakthrough-listen-sandbox").child("flask_vars").child("cache").get()
         for rc in retrieve_cache.each():
             db_cache_keys += [str(rc.key())]
         print(db_cache_keys)
      
-        if not cache:
-            print("Cache empty")
-            for key in message_dict:
-                cache[key] = get_processed_hist_and_img(message_dict[key]["object_uri"]+"/info_df.pkl")
-                db.child("breakthrough-listen-sandbox").child("flask_vars").child("cache").child(key).set(cache[key])
-        else:
-            print("cache all updated")
+       
+        for key in message_dict:
+            cache[key] = get_processed_hist_and_img(message_dict[key]["object_uri"]+"/info_df.pkl")
+            db.child("breakthrough-listen-sandbox").child("flask_vars").child("cache").child(key).set(cache[key])
+        
+        print(session["results_counter"]*3)
         return render_template("zmq_sub.html", title="Main Page", message_sub=message_dict, sample_urls = cache ,test_login = False)
 
 @app.route('/trigger')
