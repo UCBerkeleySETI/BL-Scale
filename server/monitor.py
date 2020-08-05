dec38e8e3d487b5850a4e7c23cf7ac86d72123ec
FX196 <mikechen212@gmail.com>
2020-08-05 12:51:53 -0400

    extract metrics

--- server/monitor.py
+++ server/monitor.py
@@ -29,25 +29,37 @@ for i in ret.items:
     logging.info("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
 
 api_client = client.ApiClient()
-def get_metrics(api_client):
+def get_pod_data(api_client):
     ret_metrics = api_client.call_api(
                 '/apis/metrics.k8s.io/v1beta1/namespaces/' + 'default' + '/pods', 'GET',
                 auth_settings=['BearerToken'], response_type='json', _preload_content=False)
     response = ret_metrics[0].data.decode('utf-8')
-    metrics = json.loads(response)
+    data = json.loads(response)
+    return data
+
+def extract_metrics(pod_data):
+    pods = pod_data["items"]
+    metrics = {}
+    for pod in pods:
+        pod_name = pod["metadata"]["name"]
+        metrics[pod_name]["CPU"] = pod["containers"][0]["usage"]["cpu"]
+        metrics[pod_name]["RAM"] = pod["containers"][0]["usage"]["memory"]
     return metrics
 
-metrics = get_metrics(api_client)
+pod_data = get_pod_data(api_client)
+metrics = extract_metrics(pod_data)
 logging.info(json.dumps(metrics, indent=2))
 
 
 
 while True:
     # get metrics from cluster
-    metrics = get_metrics(api_client)
+    pod_data = get_pod_data(api_client)
+    metrics = extract_metrics(pod_data)
 
     # broadcast from socket
     broadcast_socket.send_multipart([b"METRICS", pickle.dumps(metrics)])
+    logging.info(json.dumps(metrics, indent=2))
 
     # sleep 30 seconds
     time.sleep(30)
