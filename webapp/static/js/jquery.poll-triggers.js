81f2452a7dd939b72b4884433f68cc65be13e5ae
Nancy Huang <nhuang321@berkeley.edu>
2020-10-04 14:21:42 -0700

    just writing up a rough draft of a solution, needs lots of debugging

---
+++ webapp/static/js/jquery.poll-triggers.js
@@ -0,0 +1,28 @@
+(function poll() {
+
+    queryString = "state="+JSON.stringify(message_sub);
+    
+    $.ajax({
+        url:"/poll",
+        data: queryString,
+        timeout: 60000,
+        success: function() {
+            console.log("detected change");
+            update();
+            poll();
+        },
+        error: function() {
+            window.location.href = 'login.html'
+        }
+    });
+})();
+
+function update() {
+    $.ajax({
+        url:"/trigger",
+        type:"GET",
+        success: function(){
+            console.log("updated trigger")
+        }
+    });
+}
