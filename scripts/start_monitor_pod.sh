f523a838ff2b6ce6a9f809554f8f570259a99612
FX196 <mikechen212@gmail.com>
2020-11-09 20:25:15 -0500

    change run script

--- scripts/start_monitor_pod.sh
+++ scripts/start_monitor_pod.sh
@@ -1,3 +1,5 @@
+echo "Authenticating GCloud"
 gcloud auth activate-service-account --key-file=/var/run/secret/cloud.google.com/bl-scale_key.json
 gcloud container clusters get-credentials bl-scale-cluster --zone us-central1-c --project breakthrough-listen-sandbox
+echo "Starting monitor server"
 python3 server/monitor.py
