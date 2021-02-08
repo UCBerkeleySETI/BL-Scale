818d13e36b1bacef761479e015d534abd708ab87
FX196 <mikechen212@gmail.com>
2021-02-08 16:17:59 -0500

    change scheduler to 3.8

--- scripts/start_scheduler_pod.sh
+++ scripts/start_scheduler_pod.sh
@@ -1,3 +1,3 @@
 gcloud auth activate-service-account --key-file=/var/run/secret/cloud.google.com/bl-scale_key.json
 gcloud container clusters get-credentials bl-scale-cluster --zone us-central1-c --project breakthrough-listen-sandbox
-python3 server/scheduler.py
+python3.8 server/scheduler.py
