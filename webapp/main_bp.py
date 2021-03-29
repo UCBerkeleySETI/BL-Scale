ca53e1dc527ff615aea35bbaf96c5ede1879e6b9
FX196 <yuhongc@berkeley.edu>
2021-03-28 20:54:21 -0400

    remove listener import

--- webapp/main_bp.py
+++ webapp/main_bp.py
@@ -4,7 +4,6 @@ import trigger
 import logging
 from main import app
 from main import config_app
-from main import listener
 
 app.register_blueprint(monitor.bp)
 app.register_blueprint(results.bp)
@@ -14,6 +13,6 @@ if __name__ == '__main__':
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
-    
+
     app = config_app()
     app.run()
